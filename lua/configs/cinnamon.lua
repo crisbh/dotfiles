8ea04f6c057d6fb7a502b624b93cd8be31c51bfa
Cristian Barrera Hinojosa <cristian.g.barrera@durham.ac.uk>
2022-06-28 16:45:12 +0100

    tidy up nvim lua

--- lua/configs/cinnamon.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, cinnamon = pcall(require, "cinnamon")
-if status_ok then
-  cinnamon.setup(astronvim.user_plugin_opts("plugins.cinnamon", {
-      default_delay = 0 -- The default delay (in ms) between each line when scrolling
-  }))
-end
