65b0c3dc977798a73c97c44ab3ba551e41698ed9
Cristian Barrera-Hinojosa <cristian.barrera@uv.cl>
2024-04-28 20:56:49 -0400

    added bashls as default for mason

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,32 +1,33 @@
 return {
-  { 'williamboman/mason.nvim', opts = { PATH = 'append' } },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    opts = {
-      ensure_installed = {
-        'clangd',
-        'cssls',
-        'html',
-        'intelephense',
-        'marksman',
-        'neocmake',
-        'julials',
-        'pyright',
-        'lua_ls',
-        'texlab',
-        'tsserver',
-        'yamlls',
-      },
-    },
-  },
-  {
-    'jay-babu/mason-nvim-dap.nvim',
-    opts = {
-      ensure_installed = {
-        'bash',
-        'cppdbg',
-        'python',
-      },
-    },
-  },
+	{ "williamboman/mason.nvim", opts = { PATH = "append" } },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"clangd",
+				"cssls",
+				"html",
+				"intelephense",
+				"marksman",
+				"neocmake",
+				"julials",
+				"pyright",
+				"lua_ls",
+				"bashls",
+				"texlab",
+				"tsserver",
+				"yamlls",
+			},
+		},
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"cppdbg",
+				"python",
+			},
+		},
+	},
 }
