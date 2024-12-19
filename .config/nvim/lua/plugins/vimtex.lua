239d7fbd376023f67a57e621d1a3d60d1f74f024
Cristian Barrera-Hinojosa <cristian.barrera@uv.cl>
2024-12-18 22:40:10 -0300

    fixed vimtex compilation issue

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -2,12 +2,16 @@ return {
   {
     "lervag/vimtex",
     lazy = true,
+    ft = "tex",
     event = "BufEnter *.tex",
     init = function()
       vim.g.vimtex_fold_enabled = true
       vim.g.tex_flavor = "latex"
       vim.g.vimtex_view_method = "zathura"
+      -- vim.g.vimtex_viewer_options = '--fork'
+      vim.g.vimtex_viewer_options = '--synctex-forward $LINE:$COLUMN:$PDF $TEX'
       vim.g.vimtex_quickfix_mode=0
+      vim.g.vimtex_view_continuous = 1
     end,
   },
 }
