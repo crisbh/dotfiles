c733691c0c8d896cc1df334e0d52fb660ca70959
Cristian Barrera-Hinojosa <cristian.barrera@uv.cl>
2025-01-09 18:06:06 -0300

    updatex vimtex config

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -5,13 +5,21 @@ return {
     ft = "tex",
     event = "BufEnter *.tex",
     init = function()
-      vim.g.vimtex_fold_enabled = true
+      -- General settings
       vim.g.tex_flavor = "latex"
-      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_imaps_enabled = 0
+      -- vim.g.vimtex_fold_enabled = true
+      -- View settings
+      -- vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
       -- vim.g.vimtex_viewer_options = '--fork'
-      vim.g.vimtex_viewer_options = '--synctex-forward $LINE:$COLUMN:$PDF $TEX'
-      vim.g.vimtex_quickfix_mode=0
+      -- vim.g.vimtex_viewer_options = '--synctex-forward $LINE:$COLUMN:$PDF $TEX'
       vim.g.vimtex_view_continuous = 1
+      -- Quickfix settings
+      vim.g.vimtex_quickfix_mode=0
+      vim.g.vimtex_quickfix_open_on_warning = 0 --  don't open quickfix if there are only warnings
+      vim.g.vimtex_quickfix_ignore_filters = {"Underfull","Overfull", "LaTeX Warning: .\\+ float specifier changed to", "Package hyperref Warning: Token not allowed in a PDF string"}
     end,
   },
 }
