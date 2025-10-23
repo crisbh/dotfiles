7e0b107c83c8713e7d123799087fc423615eafcd
Cristian Barrera-Hinojosa <cristian.barrera@uv.cl>
2025-10-23 12:35:01 -0300

    updated to include copilot as an autocompletion source

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -8,30 +8,18 @@ return {
 				"L3MON4D3/LuaSnip",
 				lazy = true,
 				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
 					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
 						return
 					end
 					return "make install_jsregexp"
 				end)(),
 				config = function()
-					-- Lazy-load snippets, i.e. only load when required, e.g. for a given filetype
 					require("luasnip.loaders.from_lua").lazy_load({ paths = { "./lua/snippets/" } })
 					local luasnip = require("luasnip")
-					--
-					luasnip.config.set_config({ -- Setting LuaSnip config
-
-						-- Enable autotriggered snippets
+					luasnip.config.set_config({
 						enable_autosnippets = true,
-
-						-- Use Tab (or some other key if you prefer) to trigger visual selection
 						store_selection_keys = "<Tab>",
-
-						-- Update repeated nodes while typing
 						update_events = "TextChanged,TextChangedI",
-
 						history = true,
 					})
 					require("luasnip").setup({
@@ -42,28 +30,19 @@ return {
 					})
 					require("luasnip").config.setup({ enable_autosnippets = true })
 
-					-- Luasnip keymaps
 					vim.cmd([[
-            	    	    	        " Expand or jump in insert mode
-            	    	    	        " imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
-            	    	    	        " smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-					imap <silent><expr> jk luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : 'jk'
-					smap <silent><expr> jk luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : 'jk'
+            " Expand/jump shortcuts (yours)
+            imap <silent><expr> jk luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : 'jk'
+            smap <silent><expr> jk luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : 'jk'
 
+            imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+            smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 
-                			" Jump backward through snippet tabstops with Shift-Tab
-                			imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-                			smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-
-                			" Cycle forward through choice nodes with Control-f
-                			imap <silent><expr> <C-f> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-f>'
-                			smap <silent><expr> <C-f> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-f>'
-        				]])
+            imap <silent><expr> <C-f> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-f>'
+            smap <silent><expr> <C-f> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-f>'
+          ]])
 				end,
 				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
 					{
 						"rafamadriz/friendly-snippets",
 						lazy = true,
@@ -75,17 +54,19 @@ return {
 			},
 			"saadparwaiz1/cmp_luasnip",
 
-			-- Adds other completion capabilities.
-			--  nvim-cmp does not ship with all sources by default. They are split
-			--  into multiple repos for maintenance purposes.
+			-- Other sources
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
+
+			-- Copilot bridge
 			"zbirenbaum/copilot-cmp",
 		},
 		config = function()
-			-- See `:help cmp`
 			local cmp = require("cmp")
-			--- cmp setup
+			local luasnip = require("luasnip")
+			local compare = cmp.config.compare
+
+			-- nvim-cmp setup
 			cmp.setup({
 				snippet = {
 					expand = function(args)
@@ -94,38 +75,13 @@ return {
 				},
 				completion = { completeopt = "menu,menuone,noinsert" },
 
-				-- For an understanding of why these mappings were
-				-- chosen, you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
 					["<C-j>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
 					["<C-k>"] = cmp.mapping.select_prev_item(),
-
-					-- Scroll the documentation window [b]ack / [f]orward
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
 					["<Tab>"] = cmp.mapping.confirm({ select = true }),
-
-					-- Manually trigger a completion from nvim-cmp.
-					--  Generally you don't need this, because nvim-cmp will display
-					--  completions whenever it has completion options available.
 					["<C-Space>"] = cmp.mapping.complete({}),
-
-					-- Think of <c-l> as moving to the right of your snippet expansion.
-					--  So if you have a snippet that's like:
-					--  function $name($args)
-					--    $body
-					--  end
-					--
-					-- <c-l> will move you to the right of each of the expansion locations.
-					-- <c-h> is similar, except moving you backwards.
 					["<C-l>"] = cmp.mapping(function()
 						if luasnip.expand_or_locally_jumpable() then
 							luasnip.expand_or_jump()
@@ -136,16 +92,56 @@ return {
 							luasnip.jump(-1)
 						end
 					end, { "i", "s" }),
-
-					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 				}),
+
+				-- Prefer meaningful ordering; include Copilot prioritizer
+				sorting = {
+					priority_weight = 2,
+					comparators = {
+						compare.exact,
+						compare.score,
+						require("copilot_cmp.comparators").prioritize,
+						compare.recently_used,
+						compare.locality,
+						compare.offset,
+						compare.order,
+					},
+				},
+
+				-- Sources: keep LSP & snippets; cap Copilot to reduce noise
 				sources = {
-					{ name = "copilot" }, -- 👈 ensure this comes first or near the top
 					{ name = "nvim_lsp" },
+					{ name = "copilot", max_item_count = 3 },
 					{ name = "luasnip" },
 					{ name = "path" },
+					{ name = "buffer" },
 				},
+
+				-- We use cmp menu, not inline ghost text
+				experimental = { ghost_text = false },
+			})
+
+			-- Large-file guard: drop Copilot for very big buffers (>500KB)
+			vim.api.nvim_create_autocmd("BufReadPost", {
+				callback = function(ev)
+					local stat = vim.loop.fs_stat(ev.file)
+					if stat and stat.size and stat.size > 500 * 1024 then
+						cmp.setup.buffer({
+							sources = {
+								{ name = "nvim_lsp" },
+								{ name = "path" },
+								{ name = "buffer" },
+							},
+						})
+						vim.schedule(function()
+							vim.notify(
+								"󰁡 Copilot disabled for this large file",
+								vim.log.levels.INFO,
+								{ hl = "Comment" }
+							)
+						end)
+					end
+				end,
 			})
 		end,
 	},
